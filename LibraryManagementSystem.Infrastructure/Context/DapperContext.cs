cfb76c2111a8d3ef2e61bed91a07f05a540451cf
Abdulrahman <abd3lra7man.078@gmail.com>
2025-04-19 00:10:52 +0300

    Create DapperContext.

---
+++ LibraryManagementSystem.Infrastructure/Context/DapperContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace LibraryManagementSystem.Infrastructure.Context
+{
+    public class DapperContext
+    {
+        #region Instance Fields
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+        #endregion
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        }
+
+        // Method to return a database connection
+        public IDbConnection GetConnection() => new SqlConnection(_connectionString);
+    }
+}
