b81ecb338aff55a643852baf0b9327491469dfcb
Abdulrahman <abd3lra7man.078@gmail.com>
2025-04-19 00:04:38 +0300

    Create DB Context.

---
+++ LibraryManagementSystem.Infrastructure/Context/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using LibraryManagementSystem.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Infrastructure.Context
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public virtual DbSet<Book> Book { get; set; }
+        public virtual DbSet<Author> Author { get; set; }
+        public virtual DbSet<Genre> Genre { get; set; }
+        public virtual DbSet<Publisher> Publisher { get; set; }
+        public virtual DbSet<Review> BookReview { get; set; }
+        public virtual DbSet<Copy> Copy { get; set; }
+        public virtual DbSet<BorrowRequest> BorrowRequest { get; set; }
+        public ApplicationContext() { }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+        }
+    }
+}
