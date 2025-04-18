40bdd5496e193bd349bf0af4bdec35b8de11eaa3
Abdulrahman <abd3lra7man.078@gmail.com>
2025-04-19 01:08:27 +0300

    Add Migration "Update Configuration".

--- LibraryManagementSystem.Infrastructure/Migrations/20250418220746_UpdateConfigurations.Designer.cs
+++ LibraryManagementSystem.Infrastructure/Migrations/20250418220746_UpdateConfigurations.Designer.cs
@@ -4,6 +4,7 @@ using LibraryManagementSystem.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibraryManagementSystem.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20250418220746_UpdateConfigurations")]
+    partial class UpdateConfigurations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LibraryManagementSystem.Infrastructure/Migrations/20250418220746_UpdateConfigurations.cs
@@ -0,0 +1,258 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryManagementSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateConfigurations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Comment",
+                table: "BookReviews",
+                newName: "ReviewerComment");
+
+            migrationBuilder.RenameColumn(
+                name: "BookReviewerName",
+                table: "BookReviews",
+                newName: "ReviewerName");
+
+            migrationBuilder.RenameColumn(
+                name: "BookReviewTitle",
+                table: "BookReviews",
+                newName: "ReviewTitle");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BookReviews_BookReviewTitle",
+                table: "BookReviews",
+                newName: "IX_BookReviews_ReviewTitle");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PublisherName",
+                table: "Publisher",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "GenreRating",
+                table: "Genre",
+                type: "float(18)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GenreName",
+                table: "Genre",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GenreDescription",
+                table: "Genre",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ReviewerComment",
+                table: "BookReviews",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ReviewerName",
+                table: "BookReviews",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ReviewTitle",
+                table: "BookReviews",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Book",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Summary",
+                table: "Book",
+                type: "nvarchar(800)",
+                maxLength: 800,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorName",
+                table: "Author",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorBiography",
+                table: "Author",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReviewerName",
+                table: "BookReviews",
+                newName: "BookReviewerName");
+
+            migrationBuilder.RenameColumn(
+                name: "ReviewerComment",
+                table: "BookReviews",
+                newName: "Comment");
+
+            migrationBuilder.RenameColumn(
+                name: "ReviewTitle",
+                table: "BookReviews",
+                newName: "BookReviewTitle");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BookReviews_ReviewTitle",
+                table: "BookReviews",
+                newName: "IX_BookReviews_BookReviewTitle");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PublisherName",
+                table: "Publisher",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "GenreRating",
+                table: "Genre",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float(18)",
+                oldPrecision: 18,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GenreName",
+                table: "Genre",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GenreDescription",
+                table: "Genre",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BookReviewerName",
+                table: "BookReviews",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "BookReviews",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BookReviewTitle",
+                table: "BookReviews",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Book",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Summary",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(800)",
+                oldMaxLength: 800);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorName",
+                table: "Author",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorBiography",
+                table: "Author",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300);
+        }
+    }
+}
